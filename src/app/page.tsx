e6b781690c0c49be30e52e4411f363303bb80f2f
João Paulo Rodrigues <joaopaulopvai@hotmail.com>
2026-02-05 17:18:49 -0300

    Initial configurations - fix page.tsx test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,7 @@
-import Image from 'next/image';
-
-type Props = {
-  title: string;
-};
-
-const ComponentTest = ({ title }: Props) => {
-  return <h2>{title}</h2>;
-};
-
 export default function Home() {
   return (
     <div>
       <h2>Hello world!!!</h2>
-      <ComponentTest title={2} />
     </div>
   );
 }
